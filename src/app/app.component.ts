459f501b6c75c3865f39b743d1391d23c5501240
asdflkj169 <asdflkj169@gmail.com>
2024-10-07 14:16:07 +0900

    add test01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>app.component</h1>
+  `,
 })
 export class AppComponent {
   title = 'new';
---
+++ src/app/test01/test01.ts
@@ -0,0 +1,15 @@
+import {Component, NgModule} from '@angular/core';
+
+@Component({
+  selector: 'app-test01',
+  standalone: true,
+  imports: [],
+  template: '<h2>test01</h2>',
+})
+export class Test01Component {}
+
+@NgModule({
+  imports: [Test01Component],
+  exports: [Test01Component],
+})
+export class Test01Module {}
